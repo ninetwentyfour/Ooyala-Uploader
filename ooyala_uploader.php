88275b3d03024b5c6b75ffefdc1c2bebbd7d4ed4
Travis <tberry@biemedia.com>
2011-05-24 14:59:37 -0600

    got all functions working

--- ooyala_uploader.php
+++ ooyala_uploader.php
@@ -140,7 +140,7 @@ class Ooyala {
 	}
 	
 	function addToChannel($params,$options = NULL){
-		$options['urlBase'] = 'http://www.ooyala.com/partner/channel_sets';
+		$options['urlBase'] = 'http://www.ooyala.com/partner/channels';
 		$addChannelURL = Ooyala::send_request('add_channel',$params,$options);
 		$ch = curl_init($addChannelURL);
 		$postResult = curl_exec($ch);
@@ -154,14 +154,14 @@ class Ooyala {
 	
 	//generate the signatures and format the request url
 	private static function send_request($request_type, $params, $options){
-		$ooyala_pcode = 'PARTNER CODE HERE';
-		$ooyala_scode = 'SECRET CODE HERE';
+		$ooyala_pcode = 'FqY2o6IaPQg4mSYPUApYy0LgwE_p';
+		$ooyala_scode = 't8u-Ed-aDLlVecI2C4wDfhqPbbcXXDwZa2sAJtsw';
 		// Add an expire time of 15 minutes unless otherwise specified
 		if (!array_key_exists('expires', $params)) {
 			$params['expires'] = time() + 900;
 		}
 		$string_to_sign = $ooyala_scode;
-		if($options['urlBase'] == 'http://uploader.ooyala.com/api/upload/preview' || $options['urlBase'] == 'http://uploader.ooyala.com/api/upload/preview'){
+		if($options['urlBase'] == 'http://uploader.ooyala.com/api/upload/preview' || $options['urlBase'] == 'http://www.ooyala.com/partner/channels'){
 			$urlBase = $options['urlBase'];
 		}else{
 			$urlBase = $options['urlBase'].$request_type;
@@ -203,7 +203,7 @@ $needsEndDate = 'true'; //true or false if using end date
 
 $rootLabel = '/Development/';//everything up to the last label e.g. /SuperPages/
 
-$channel = 'none'; // set to none if no need to assign video to a channel or use channel embed code
+$channel = 'ZycHhnMjqV_b7lcM3fdQ1UVF4BoNz-dE'; // set to none if no need to assign video to a channel or use channel embed code
 
 
 //get all posted data
@@ -303,7 +303,7 @@ if($needsThumbnail == 'true'){
 
 // add video to channel if needed
 if($channel != 'none'){
-	$addToChannel = Ooyala::addToChannel(array('channelSetEmbedCode' => $channel, 'channelEmbedCodes' => $embedCode , 'mode' => 'assign'));
+	$addToChannel = Ooyala::addToChannel(array('channelEmbedCode' => $channel, 'embedCodes' => $embedCode , 'mode' => 'assign'));
 	if($addToChannel != 'pass'){
 		//throw error not assigned to channel
 		$error = 'Video was not assigned to the Channel. Try Again.';
